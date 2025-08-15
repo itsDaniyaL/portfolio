03bcf7aa47f594d7815b8fde7b221c84e53e8d42
Daniyal Ahmad Rizwan <daniyal.ahmad.rizwan@hotmail.com>
2025-08-15 05:09:47 +0500

    working on carousel, trying to perfect the style

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, InputAdornment, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import EastIcon from "@mui/icons-material/East";
-import { East } from "@mui/icons-material";
+import { useState } from "react";
 
 const items = [
   { id: 1, title: "Item 1", bg: "#FF5733" },
@@ -12,9 +12,34 @@ const items = [
 ];
 
 export default function Projects() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const boxes = [0, 1, 2];
+
+  const getVisibleBoxes = () => {
+    if (boxes.length <= 3) {
+      return boxes;
+    }
+    let start = Math.max(0, activeIndex - 1);
+    let end = start + 3;
+
+    if (end > boxes.length) {
+      end = boxes.length;
+      start = end - 3;
+    }
+
+    return boxes.slice(start, end);
+  };
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-      <Box sx={{ margin: "75px 10px 0 10px" }}>
+      <Box
+        sx={{
+          margin: "75px 10px 0 10px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
         <TextField
           id="outlined-basic"
           variant="outlined"
@@ -27,119 +52,129 @@ export default function Projects() {
             ),
           }}
         />
+        <p
+          style={{ color: "#E6E1E3", fontSize: "24px", alignContent: "center" }}
+        >
+          10 Projects
+        </p>
       </Box>
       <Box sx={{ flexGrow: 1, display: "flex" }}>
-        <Box
-          sx={{
-            flex: 4,
-            margin: "10px",
-            background: "#384739",
-            borderRadius: "25px",
-            display: "flex",
-          }}
-        >
+        {getVisibleBoxes().map((index) => (
           <Box
+            key={index}
+            {...(activeIndex !== index && {
+              onClick: () => setActiveIndex(index),
+            })}
             sx={{
-              flex: 1,
+              flex: activeIndex === index ? 4 : 1,
+              transition: "flex 0.4s ease",
+              minWidth: activeIndex === index ? "auto" : "150px",
+              margin: "10px",
+              background: "#384739",
+              borderRadius: "25px",
               display: "flex",
-              flexDirection: "column",
-              borderRadius: "15px",
-              margin: "20px 10px 20px 20px",
+              cursor: activeIndex === index ? "auto" : "pointer",
             }}
           >
             <Box
               sx={{
+                flex: 1,
                 display: "flex",
                 flexDirection: "column",
-                justifyContent: "space-between",
-                height: "100%",
+                borderRadius: "15px",
+                margin: "20px",
               }}
             >
-              <Box>
-                <Box sx={{ display: "flex" }}>
-                  <img
-                    src={require("../assets/khata_icon.png")}
-                    style={{ height: "50px" }}
-                  ></img>
-                  <h1
-                    style={{
-                      fontSize: "18px",
-                      color: "#E6E1E3",
-                      marginLeft: "20px",
-                      fontWeight: "bold",
-                      alignContent: "center",
-                    }}
-                  >
-                    Khata at Home
-                  </h1>
-                </Box>
-                <h1 style={{ color: "#E6E1E3", margin: "20px 0" }}>
-                  Take control of your Khata, take control of your life.
-                </h1>
-              </Box>
-              <Box>
-                <Button
-                  variant="contained"
-                  sx={{
-                    background: "#1D221D",
-                    width: "150px",
-                    height: "50px",
-                    borderRadius: "15px",
-                    display: "flex",
-                    flexDirection: "column",
-                    textTransform: "none",
-                    alignItems: "flex-start",
-                  }}
-                >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  height: "100%",
+                }}
+              >
+                <Box>
                   <Box sx={{ display: "flex" }}>
-                    <p
+                    <img
+                      src={require("../assets/khata_icon.png")}
+                      style={{ height: "50px" }}
+                    ></img>
+                    <h1
                       style={{
-                        fontSize: "10px",
-                        marginRight: "5px",
+                        fontSize: "18px",
                         color: "#E6E1E3",
+                        marginLeft: "20px",
+                        fontWeight: "bold",
+                        alignContent: "center",
                       }}
                     >
-                      Read more
-                    </p>
-                    <EastIcon sx={{ fontSize: "15px" }} />
+                      Khata at Home
+                    </h1>
                   </Box>
-                  <p style={{ color: "#E6E1E3", fontWeight: "bold" }}>
-                    Project Details
-                  </p>
-                </Button>
+                  <h1 style={{ color: "#E6E1E3", margin: "20px 0" }}>
+                    Take control of your Khata, take control of your life.
+                  </h1>
+                </Box>
+                {activeIndex === index ? (
+                  <Box>
+                    <Button
+                      variant="contained"
+                      sx={{
+                        background: "#1D221D",
+                        width: "150px",
+                        height: "50px",
+                        borderRadius: "15px",
+                        display: "flex",
+                        flexDirection: "column",
+                        textTransform: "none",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box sx={{ display: "flex" }}>
+                        <p
+                          style={{
+                            fontSize: "10px",
+                            marginRight: "5px",
+                            color: "#E6E1E3",
+                          }}
+                        >
+                          Read more
+                        </p>
+                        <EastIcon sx={{ fontSize: "15px" }} />
+                      </Box>
+                      <p style={{ color: "#E6E1E3", fontWeight: "bold" }}>
+                        Project Details
+                      </p>
+                    </Button>
+                  </Box>
+                ) : null}
               </Box>
             </Box>
+            {activeIndex === index ? (
+              <Box
+                sx={{
+                  flex: 1,
+                  display: "flex",
+                  background: "#1D221D",
+                  borderRadius: "15px",
+                  margin: "20px 20px 20px 10px",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  src={require("../assets/khata-app-1.png")}
+                  alt="Khata App"
+                  style={{
+                    width: "90%",
+                    height: "90%",
+                    objectFit: "contain",
+                  }}
+                />
+              </Box>
+            ) : null}
           </Box>
-          <Box
-            sx={{
-              flex: 1,
-              display: "flex",
-              background: "#1D221D",
-              borderRadius: "15px",
-              margin: "20px 20px 20px 10px",
-            }}
-          ></Box>
-        </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-            margin: "10px",
-            background: "#384739",
-            borderRadius: "25px",
-          }}
-        >
-          <Box></Box>
-        </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-            margin: "10px",
-            background: "#384739",
-            borderRadius: "25px",
-          }}
-        >
-          <Box></Box>
-        </Box>
+        ))}
       </Box>
     </Box>
   );
